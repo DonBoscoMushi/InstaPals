a3cd879f403be1ee4f0665cc839ed937fdadbd53
donnicholaus@gmail.com <gaetanamD13>
2020-08-07 13:32:42 +0300

    Grtid view of posts in profile added ... Trying to fix the view of pictures

--- app/src/main/java/com/donnicholaus/instapals/profile/ProfileActivity.java
+++ app/src/main/java/com/donnicholaus/instapals/profile/ProfileActivity.java
@@ -1,9 +1,11 @@
 package com.donnicholaus.instapals.profile;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -14,14 +16,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.donnicholaus.instapals.R;
 import com.donnicholaus.instapals.util.BottomNavHelper;
+import com.donnicholaus.instapals.util.GridImageAdapter;
 import com.donnicholaus.instapals.util.UniversalImageLoader;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private ProgressBar progressBar;
+    private Context mContext = ProfileActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,8 @@ public class ProfileActivity extends AppCompatActivity {
         setupBottomNav();
         setupToolBar();
         setProfileImage();
+
+        tempGrid();
     }
 
     private void setupToolBar(){
@@ -83,4 +91,35 @@ public class ProfileActivity extends AppCompatActivity {
             UniversalImageLoader.setImage(imgURL, imageView, progressBar, "https://");
     }
 
+
+    private void setImageGrid(ArrayList<String> imgURLs){
+        GridView gridView = findViewById(R.id.gridView);
+        GridImageAdapter gridImageAdapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
+        gridView.setAdapter(gridImageAdapter);
+    }
+
+    private void tempGrid(){
+        ArrayList<String> imgURLs = new ArrayList<>();
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/arctichare.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/baboon.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/barbara.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/boat.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/cat.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/fruits.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/girl.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/goldhill.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/lena.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/monarch.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/mountain.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/peppers.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/pool.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/sails.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/serrano.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/tulips.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/watch.png");
+        imgURLs.add("https://homepages.cae.wisc.edu/~ece533/images/zelda.png");
+
+        setImageGrid(imgURLs);
+    }
 }
